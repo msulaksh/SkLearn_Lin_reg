986c742eec9ef570e6fa50c5f9fdf21602310783
Sulaksh M More <38561021+msulaksh@users.noreply.github.com>
2019-04-10 18:55:15 +0530

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -1,1 +1,8 @@
+from pandas import read_csv
 
+boston = read_csv(r"filename.csv", header=0)
+print(boston.values)
+print(boston.columns)
+print(boston.head)
+print(boston.info)
+print(boston.describe)
